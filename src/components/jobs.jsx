96a3573c4fd84858d889f0ce56c9a7b240a22180
Matthew <matthew.b.rodriguez@gmail.com>
2019-12-07 13:35:15 -0500

    Jobs static page 1.0 done. TODO: Add dynamic functionality

--- src/components/jobs.jsx
+++ src/components/jobs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Form, FormControl, Button, Container } from 'react-bootstrap';
+import { Row, Col, Form, FormControl, Button, Container, Modal } from 'react-bootstrap';
 
 class JobHeader extends React.Component {
 	render() {
@@ -49,6 +49,47 @@ class JobPosting extends React.Component {
 			fontSize: '18px',
 			fontWeight:'bold'
 		};
+		
+		// TODO: Make this dynamic using props and state
+		function LearnMore() {
+		  const [show, setShow] = React.useState(false);
+
+		  const handleClose = () => setShow(false);
+		  const handleShow = () => setShow(true);
+
+		  return (
+		    <>
+		      <Button className="float-right" variant="outline-info" onClick={handleShow}>
+		        Learn More
+		      </Button>
+
+		      <Modal show={show} onHide={handleClose}>
+		        <Modal.Header closeButton>
+		          <Modal.Title>Logo Design</Modal.Title>
+		        </Modal.Header>
+		        <Modal.Body>
+							<div>Poster: <span>Rageeb Mahtab</span></div>
+							<div>Location: <span>New York, NY</span></div>
+							<div>Posted: <span>12/07/2019</span></div>
+							<div>Pay Rate: <span>$100</span></div>
+
+							<br />
+							<p>
+								Basically, I am creating a startup. We need an amazing logo designer for 1 day. For this job, you will be doing mockups, and helping us build our brand image. Message for further inquiry. Availability needed withint next two weeks.
+							</p>
+						</Modal.Body>
+		        <Modal.Footer>
+		          <Button variant="secondary" onClick={handleClose}>
+		            Close
+		          </Button>
+		          <Button variant="primary" onClick={handleClose}>
+		            Apply
+		          </Button>
+		        </Modal.Footer>
+		      </Modal>
+		    </>
+		  );
+		}
 
 		return (
 			<Container className="border border-light rounded">
@@ -87,12 +128,19 @@ class JobPosting extends React.Component {
 						<span style={ payRate }>$100</span>
 					</Col>
 				</Row>
+
+				<Row>
+					<Col className="border-top border-light pt-2 pb-2">
+						<LearnMore />
+					</Col>
+				</Row>
 			</Container>
 		);
 	}
 }
 
 
+// TODO: Use for loop to populate <JobPosting />s inside Container
 export class Jobs extends React.Component {
 	render() {
 		return (
